b3838f16b353c7060db6811ac0d75eea115fb758
joshuashevchuk <joshua.shevchuk@ookla.com>
2023-11-07 15:03:31 -0800

    connect to mongo with conn.js

--- server/db/conn.js
+++ server/db/conn.js
@@ -6,13 +6,8 @@ var _db;
 
 module.exports = {
     connectToServer: function (callback) {
-        client.connect(function (err, db) {
-            // Verify we got a good "db" object
-            if (db)
-            {
-
+        client.connect(function (err) {
                 console.log("Successfully connected to MongoDB.");
-            }
             return callback(err);
         });
     },

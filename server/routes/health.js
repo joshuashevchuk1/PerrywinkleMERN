ce630e34b681a0b7788af2bbee3780fbe7d13681
joshuashevchuk <joshua.shevchuk@ookla.com>
2023-11-07 15:51:25 -0800

    add a simple health check for the server

---
+++ server/routes/health.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const health = express.Router();
+
+// This will help us connect to the database
+const dbo = require("../db/conn");
+
+// This help convert the id from string to ObjectId for the _id.
+const ObjectId = require("mongodb").ObjectId;
+
+
+health.route("/").get(function (req,res){
+    res.send("OK");
+});
+
+module.exports = health;

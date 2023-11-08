2f88bfa13e3deffdebe87c23b716ffec580c817f
joshuashevchuk <joshua.shevchuk@ookla.com>
2023-11-08 09:21:33 -0800

    remove used code in health.route.js

--- server/routes/health.route.js
+++ server/routes/health.route.js
@@ -1,13 +1,6 @@
 const express = require("express");
 const healthRoute = express.Router();
 
-// This will help us connect to the database
-const dbo = require("../db/db");
-
-// This help convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
-
-
 healthRoute.route("/health").get(function (req, res){
     res.send("OK");
 });
